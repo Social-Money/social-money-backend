0620aff51a7bf90b6a74dca5a8bc682ed39e7502
shellteo <shellteo@163.com>
2020-05-15 17:08:18 +0800

    update contract address

--- app/contract/index.js
+++ app/contract/index.js
@@ -7,7 +7,7 @@ module.exports = {
   SocialMoneyFactoryABI: SocialMoneyFactoryJSON.abi,
   UniswapFactoryABI,
   SocialMoneyABI: SocialMoneyJSON.abi,
-  SocialMoneyFactoryAddress: '0x57DAE729e185B0b7e22636F6d4D3214c800C086b',
-  UniswapFactoryAddress: '0x09cc1a1c32b4adcff81fa3b44a142d6e023bc2eb',
+  SocialMoneyFactoryAddress: '0x9d21FDB292e69F3EF3d5De96D92248d314003fca',
+  UniswapFactoryAddress: '0xc4d477bcf1578ebf4dee2318e19ab7649a83f5bb',
   infuraKey: '2e935bd97a964208a7009f6c47b27757',
 };
